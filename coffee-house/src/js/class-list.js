b6a1f7b0ceb59eb6230cbeb30fa17f723f68d827
Kleostro <maxim_zabaluev@icloud.com>
2023-12-17 12:45:58 +0300

    docs: delete unnecessary files

--- coffee-house/src/js/class-list.js
+++
@@ -1,9 +0,0 @@
-const CLASS_LIST = {
-  // burger.js
-  burgerActive: 'burger--active',
-  menuActive: 'header__left--active',
-  navActive: 'nav--active',
-  stopScroll: 'stop-scroll',
-};
-
-export default CLASS_LIST;
