aae1841259c7fc4e12c730c293a0bfa0645ecf91
Kleostro <maxim_zabaluev@icloud.com>
2023-12-17 12:16:35 +0300

    docs: replace build assembly with dev

---
+++ coffee-house/menu/src/js/class-list.js
@@ -0,0 +1,23 @@
+const CLASS_LIST = {
+  // burger.js
+  burgerActive: 'burger--active',
+  menuActive: 'header__left--active',
+  navActive: 'nav--active',
+  stopScroll: 'stop-scroll',
+
+  // menu-list.js
+  menuBtnAddVisible: 'menu__btn-add--visible',
+  menuListItemHidden: 'menu__list-item--hidden',
+  menuModalSizeBtnActive: 'menu__modal-size-btn--active',
+  menuModalSizeNameActive: 'menu__modal-size-name--active',
+  menuModalAdditivesBtnActive: 'menu__modal-additives-btn--active',
+  menuModalAdditivesIndexActive: 'menu__modal-additives-index--active',
+  menuTabsBtnActive: 'menu__tabs-btn--active',
+
+  // modal.js
+  modalVisible: 'modal--visible',
+  modalOverlayVisible: 'modal__overlay--visible',
+  modalContentVisible: 'modal__content--visible',
+};
+
+export default CLASS_LIST;
