4e9482870aa2d1dfe90c0bce70c0e96772db9dc5
Kleostro <maxim_zabaluev@icloud.com>
2023-12-28 02:04:16 +0300

    refactor: move all quiz items and files to a separate component

--- hangman/src/js/components/game/game.js
+++ hangman/src/js/components/game/game.js
@@ -1,5 +1,6 @@
 import BaseCreateElement from '../../BaseCreateElement';
 import gallowsElem from './components/gallows/gallows';
+import quizBoxElem from './components/quiz/quiz';
 import { checkLetter } from './gameStates';
 
 const gameSection = new BaseCreateElement('section', ['game']);
@@ -11,18 +12,6 @@ const gameContainerElem = gameContainer.elem;
 const keyboardBox = new BaseCreateElement('div', ['keyboard']);
 const keyboardBoxElem = keyboardBox.elem;
 
-const quizBox = new BaseCreateElement('div', ['quiz']);
-const quizBoxElem = quizBox.elem;
-
-const questionTitle = new BaseCreateElement('h2', ['quiz__question']);
-const questionTitleElem = questionTitle.elem;
-
-const wrongGuess = new BaseCreateElement('span', ['quiz__wrong']);
-const wrongGuessElem = wrongGuess.elem;
-
-const answerBox = new BaseCreateElement('div', ['quiz__answer']);
-const answerBoxElem = answerBox.elem;
-
 for (let i = 65; i <= 90; i += 1) {
   const keyboardBtn = new BaseCreateElement('button', ['btn-reset', 'keyboard__btn']);
   const keyboardBtnElem = keyboardBtn.elem;
@@ -32,7 +21,6 @@ for (let i = 65; i <= 90; i += 1) {
   keyboardBoxElem.append(keyboardBtnElem);
 }
 
-quizBoxElem.append(questionTitleElem, wrongGuessElem, answerBoxElem);
 gameContainerElem.append(gallowsElem, quizBoxElem, keyboardBoxElem);
 gameSectionElem.append(gameContainerElem);
 export default gameSectionElem;
