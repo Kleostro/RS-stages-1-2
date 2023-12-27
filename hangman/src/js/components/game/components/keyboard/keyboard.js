0fcb5d1758770171e55ebfc1a05b5209c7ea8615
Kleostro <maxim_zabaluev@icloud.com>
2023-12-28 02:06:32 +0300

    refactor: move all elements and keyboard files to a separate component

---
+++ hangman/src/js/components/game/components/keyboard/keyboard.js
@@ -0,0 +1,16 @@
+import BaseCreateElement from '../../../../BaseCreateElement';
+import { checkLetter } from '../../gameStates';
+
+const keyboardBox = new BaseCreateElement('div', ['keyboard']);
+const keyboardBoxElem = keyboardBox.elem;
+
+for (let i = 65; i <= 90; i += 1) {
+  const keyboardBtn = new BaseCreateElement('button', ['btn-reset', 'keyboard__btn']);
+  const keyboardBtnElem = keyboardBtn.elem;
+  const btnLetter = String.fromCharCode(i);
+  keyboardBtnElem.textContent = btnLetter;
+  keyboardBtnElem.addEventListener('click', (e) => checkLetter(e.target, btnLetter));
+  keyboardBoxElem.append(keyboardBtnElem);
+}
+
+export default keyboardBoxElem;
--- hangman/src/js/components/game/game.js
+++ hangman/src/js/components/game/game.js
@@ -1,25 +1,13 @@
 import BaseCreateElement from '../../BaseCreateElement';
 import gallowsElem from './components/gallows/gallows';
+import keyboardBoxElem from './components/keyboard/keyboard';
 import quizBoxElem from './components/quiz/quiz';
-import { checkLetter } from './gameStates';
 
 const gameSection = new BaseCreateElement('section', ['game']);
 const gameSectionElem = gameSection.elem;
 
 const gameContainer = new BaseCreateElement('div', ['container', 'game__container']);
 const gameContainerElem = gameContainer.elem;
-
-const keyboardBox = new BaseCreateElement('div', ['keyboard']);
-const keyboardBoxElem = keyboardBox.elem;
-
-for (let i = 65; i <= 90; i += 1) {
-  const keyboardBtn = new BaseCreateElement('button', ['btn-reset', 'keyboard__btn']);
-  const keyboardBtnElem = keyboardBtn.elem;
-  const btnLetter = String.fromCharCode(i);
-  keyboardBtnElem.textContent = btnLetter;
-  keyboardBtnElem.addEventListener('click', (e) => checkLetter(e.target, btnLetter));
-  keyboardBoxElem.append(keyboardBtnElem);
-}
 
 gameContainerElem.append(gallowsElem, quizBoxElem, keyboardBoxElem);
 gameSectionElem.append(gameContainerElem);
