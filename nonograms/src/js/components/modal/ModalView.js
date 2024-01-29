631d90bcd276a1ea787c3967f0d8e4d134a788d5
Kleostro <maxim_zabaluev@icloud.com>
2024-01-29 22:37:42 +0300

    refactor: reorganize the modal window into class

---
+++ nonograms/src/js/components/modal/ModalView.js
@@ -0,0 +1,54 @@
+import CreateElement from '../../CreateElement';
+import './modalView.scss';
+
+class ModalView {
+  constructor() {
+    this.message = null;
+    this.name = null;
+    this.time = null;
+
+    this.#createHTML()
+
+    this.closeBtn.addEventListener('click', () => this.show(this.message, this.name, this.time));
+
+    this.overlay.addEventListener('click', ({ target }) => {
+      if (target === this.overlay) {
+        this.show(this.message, this.name, this.time);
+      }
+    });
+  }
+
+  getHTML() {
+    return this.modalBox;
+  }
+
+  show(message, name, time) {
+    this.message = message;
+    this.name = name;
+    this.time = time;
+    const { formattedMin, formattedSec } = this.time;
+
+    this.title.innerHTML = `${this.message}<span class="modal__title--accent">${this.name}</span>`;
+    this.subtitle.innerHTML = `Time: <span class="modal__subtitle--accent">${formattedMin}:${formattedSec}</span>`;
+
+    this.modalBox.classList.toggle('hidden');
+    this.overlay.classList.toggle('hidden');
+    this.content.classList.toggle('hidden');
+    document.body.classList.toggle('stop-scroll');
+  }
+
+  #createHTML() {
+    this.modalBox = new CreateElement({ classes: ['modal'] });
+    this.overlay = new CreateElement({ classes: ['modal__overlay'] });
+    this.content = new CreateElement({ classes: ['modal__content'] });
+    this.title = new CreateElement({ tag: 'h3', classes: ['modal__title'] });
+    this.subtitle = new CreateElement({ tag: 'h3', classes: ['modal__subtitle'] });
+    this.closeBtn = new CreateElement({ tag: 'btn', classes: ['btn-reset', 'modal__close-btn'] });
+
+    this.content.append(this.title, this.subtitle, this.closeBtn);
+    this.overlay.append(this.content);
+    this.modalBox.append(this.overlay);
+  }
+}
+
+export default ModalView;
