982eaf9ddf3a9e174f4849c21f64e6b9b163d8d1
Kleostro <maxim_zabaluev@icloud.com>
2024-01-30 04:56:03 +0300

    feat: finalize the winner's bracket

--- nonograms/src/js/components/winners/WinnersView.js
+++ nonograms/src/js/components/winners/WinnersView.js
@@ -1,6 +1,10 @@
 import CreateElement from '../../CreateElement';
 import './winnersView.scss';
 
+const MAX_MS_IN_SEC = 60;
+const MAX_SEC_IN_MIN = 60;
+
+const MAX_WINNERS = 5;
 class WinnersView {
   constructor() {
     this.#createHTML();
@@ -32,15 +36,51 @@ class WinnersView {
       time: params[2]
     }
     const LS = JSON.parse(localStorage.getItem('kleostro'));
-    LS['winners'].push(winner);
+    if (LS.winners.length >= MAX_WINNERS) {
+      LS.winners.shift();
+      LS.winners.push(winner);
+    } else {
+      LS.winners.push(winner);
+    }
     localStorage.setItem('kleostro', JSON.stringify(LS));
+    this.sortWinners();
+  }
+
+  sortWinners() {
+    const LS = JSON.parse(localStorage.getItem('kleostro'));
+    return LS.winners.sort((a, b) => a.time - b.time);
   }
 
   show() {
+    this.winnersList.innerHTML = '';
     this.winnersBox.classList.toggle('hidden');
     this.overlay.classList.toggle('hidden');
     this.content.classList.toggle('hidden');
     document.body.classList.toggle('stop-scroll');
+
+    const sortedListWinners = this.sortWinners();
+
+    const listHeader = new CreateElement({ tag: 'li', classes: ['winners-modal__list-header'] });
+    const listIndex = new CreateElement({ tag: 'li', classes: ['winners-modal__list-header'], textContent: '№' });
+    const listTitle = new CreateElement({ tag: 'li', classes: ['winners-modal__list-header'], textContent: 'Name' });
+    const listSize = new CreateElement({ tag: 'li', classes: ['winners-modal__list-header'], textContent: 'Size' });
+    const listTime = new CreateElement({ tag: 'li', classes: ['winners-modal__list-header'], textContent: 'Time' });
+
+    this.winnersList.append(listHeader, listIndex, listTitle, listSize, listTime);
+
+    sortedListWinners.forEach((winner, index) => {
+      const formattedMin = Math.floor(winner.time / MAX_SEC_IN_MIN).toString().padStart(2, '0');
+      const formattedSec = (winner.time % MAX_MS_IN_SEC).toString().padStart(2, '0');
+
+      const listItem = new CreateElement({ tag: 'li', classes: ['winners-modal__list-item'] });
+      const winnerIndex = new CreateElement({ tag: 'span', classes: ['winners-modal__list-index'], textContent: index += 1 });
+      const winnerTitle = new CreateElement({ tag: 'span', classes: ['winners-modal__list-title'], textContent: winner.title });
+      const winnerSize = new CreateElement({ tag: 'span', classes: ['winners-modal__list-size'], textContent: winner.size });
+      const winnerTime = new CreateElement({ tag: 'span', classes: ['winners-modal__list-time'], textContent: `${formattedMin}:${formattedSec}` });
+
+      listItem.append(winnerIndex, winnerTitle, winnerSize, winnerTime);
+      this.winnersList.append(listItem);
+    });
   }
 
   #createHTML() {
@@ -50,7 +90,9 @@ class WinnersView {
     this.title = new CreateElement({ tag: 'h3', classes: ['winners-modal__title'], textContent: 'List of winners' });
     this.closeBtn = new CreateElement({ tag: 'btn', classes: ['btn-reset', 'winners-modal__close-btn'] });
 
-    this.content.append(this.title, this.closeBtn);
+    this.winnersList = new CreateElement({ tag: 'ul', classes: ['list-reset', 'winners-modal__list'] });
+
+    this.content.append(this.title, this.winnersList, this.closeBtn);
     this.overlay.append(this.content);
     this.winnersBox.append(this.overlay);
   }
