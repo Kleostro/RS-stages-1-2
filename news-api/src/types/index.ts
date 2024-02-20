8fa609f3cabe0db8364523fbf3df58c299d049a4
Kleostro <maxim_zabaluev@icloud.com>
2024-02-20 18:10:45 +0300

    feat: add an interface for news

---
+++ news-api/src/types/index.ts
@@ -0,0 +1,67 @@
+type Category = 'business' | 'entertainment' | 'general' | 'health' | 'science' | 'sports' | 'technology';
+type Language = 'ar' | 'de' | 'en' | 'es' | 'fr' | 'he' | 'it' | 'nl' | 'no' | 'pt' | 'ru' | 'sv' | 'ud' | 'zh';
+type Country =
+  | 'ae'
+  | 'ar'
+  | 'at'
+  | 'au'
+  | 'be'
+  | 'bg'
+  | 'br'
+  | 'ca'
+  | 'ch'
+  | 'cn'
+  | 'co'
+  | 'cu'
+  | 'cz'
+  | 'de'
+  | 'eg'
+  | 'fr'
+  | 'gb'
+  | 'gr'
+  | 'hk'
+  | 'hu'
+  | 'id'
+  | 'ie'
+  | 'il'
+  | 'in'
+  | 'it'
+  | 'jp'
+  | 'kr'
+  | 'lt'
+  | 'lv'
+  | 'ma'
+  | 'mx'
+  | 'my'
+  | 'ng'
+  | 'nl'
+  | 'no'
+  | 'nz'
+  | 'ph'
+  | 'pl'
+  | 'pt'
+  | 'ro'
+  | 'rs'
+  | 'ru'
+  | 'sa'
+  | 'se'
+  | 'sg'
+  | 'si'
+  | 'sk'
+  | 'th'
+  | 'tr'
+  | 'tw'
+  | 'ua'
+  | 'us'
+  | 've'
+  | 'za';
+
+export interface News {
+  id: string;
+  name: string;
+  description: string;
+  url: string;
+  category: Category;
+  language: Language;
+  country: Country;
+}
