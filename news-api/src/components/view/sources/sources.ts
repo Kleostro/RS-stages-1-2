43c04631b3c0f16f80d8b6598cd4f01703324c1a
Kleostro <maxim_zabaluev@icloud.com>
2024-02-21 16:41:07 +0300

    refactor: change the creation of elements through the auxiliary function

--- news-api/src/components/view/sources/sources.ts
+++ news-api/src/components/view/sources/sources.ts
@@ -1,5 +1,6 @@
 import './sources.css';
 import type { SourcesDataInterface } from '@/types';
+import * as Utilities from '@/utilities';
 
 interface SourcesClassInterface {
   draw(data: SourcesDataInterface[]): void;
@@ -11,20 +12,14 @@ class Sources implements SourcesClassInterface {
   }
 
   private _createSourceItem(item: SourcesDataInterface): void {
-    const sourcesWrapper = document.querySelector('.sources');
-    const sourceItem = document.createElement('div');
-    sourceItem.classList.add('source__item');
-    sourceItem.setAttribute('data-source-id', item.id);
+    const sourcesWrapper = Utilities.safeQuerySelector('.sources');
+    const sourceItem = Utilities.createElement('div', ['source__item'], { 'data-source-id': item.id });
 
-    const sourceItemName = document.createElement('span');
-    sourceItemName.classList.add('source__item-name');
+    const sourceItemName = Utilities.createElement('span', ['source__item-name']);
     sourceItemName.textContent = item.name;
 
     sourceItem.append(sourceItemName);
-
-    if (sourcesWrapper) {
-      sourcesWrapper.append(sourceItem);
-    }
+    sourcesWrapper.append(sourceItem);
   }
 }
 
