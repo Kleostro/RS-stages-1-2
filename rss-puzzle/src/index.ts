d2291f2a155f71c491d1b278d7014af30f7276b3
Max <133934232+Kleostro@users.noreply.github.com>
2024-03-05 08:23:17 +0300

    chore: RSS-PZ-34 Init Config (#150)

---
+++ rss-puzzle/src/index.ts
@@ -0,0 +1,5 @@
+import './index.scss';
+// import App from './app/app.ts';
+
+// const myApp = new App();
+// document.body.append(myApp.app);
---
+++ rss-puzzle/vite.config.js
@@ -0,0 +1,45 @@
+import Inspect from 'vite-plugin-inspect';
+import { resolve } from 'path';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import autoprefixer from 'autoprefixer';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import checker from 'vite-plugin-checker';
+
+export default {
+  publicDir: 'assets',
+  plugins: [
+    Inspect(),
+    ViteImageOptimizer({
+      png: {
+        quality: 85,
+      },
+      jpg: {
+        quality: 85,
+      },
+      webp: {
+        quality: 70,
+      },
+    }),
+    tsconfigPaths(),
+    checker({
+      typescript: true,
+    }),
+  ],
+  base: './',
+  root: 'src',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, './src/index.html'),
+      },
+    },
+    minify: false,
+    compact: false,
+    sourcemap: true,
+  },
+  css: {
+    postcss: {
+      plugins: [autoprefixer()],
+    },
+  },
+};
