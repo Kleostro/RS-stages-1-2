f8b33749809ffa062a004d3a8b6f57b66b9f2c07
Max <133934232+Kleostro@users.noreply.github.com>
2024-03-05 08:37:33 +0300

    feat: RSS-PZ-36 Add Login Form (#152)

---
+++ rss-puzzle/src/widgets/loginForm/LoginForm.ts
@@ -0,0 +1,136 @@
+import styles from './style.module.scss';
+import createBaseElement from '../../utils/createBaseElement.ts';
+import InputFieldComponent from '../../entities/inputField/InputField.ts';
+import SubmitButtonComponent from '../../entities/submitBtn/SubmitBtn.ts';
+import FormValidation from '../../features/formValidation/FormValidation.ts';
+import FIELD_NAMES from './types/enum.ts';
+
+class LoginForm {
+  private form: HTMLFormElement;
+
+  protected inputFields: InputFieldComponent[] = [];
+
+  protected inputFieldsHTML: HTMLInputElement[] = [];
+
+  protected fieldErrors: HTMLSpanElement[] = [];
+
+  protected submitBtn: SubmitButtonComponent;
+
+  private userData: { [key: string]: FormDataEntryValue | null } = {};
+
+  private formValidation: FormValidation;
+
+  constructor() {
+    this.submitBtn = this.createSubmitBtn();
+    this.form = this.createHTML();
+    this.form.addEventListener('submit', this.submit.bind(this));
+    this.formValidation = new FormValidation(
+      this.inputFields,
+      this.fieldErrors,
+      this.submitBtn.getHTML(),
+    );
+    this.formValidation.initValidation();
+  }
+
+  public getHTML(): HTMLFormElement {
+    return this.form;
+  }
+
+  private submit(event: Event): void {
+    event.preventDefault();
+    const formData = new FormData(this.form);
+    const nameData = formData.get(FIELD_NAMES.NAME);
+    const surnameData = formData.get(FIELD_NAMES.SURNAME);
+    this.userData.name = nameData;
+    this.userData.surname = surnameData;
+  }
+
+  private createFieldBox(input: HTMLInputElement): HTMLLabelElement {
+    const fieldLabel = createBaseElement({
+      tag: 'label',
+      cssClasses: [styles.form__label],
+      attributes: {
+        for: input.id,
+      },
+      innerContent: `Enter ${input.name}`,
+    });
+
+    const fieldErrorSpan = createBaseElement({
+      tag: 'span',
+      cssClasses: [styles.form__span],
+      attributes: {
+        id: input.id,
+      },
+    });
+
+    this.fieldErrors.push(fieldErrorSpan);
+    fieldLabel.append(input, fieldErrorSpan);
+    return fieldLabel;
+  }
+
+  private createInputsField(): InputFieldComponent[] {
+    const inputName = new InputFieldComponent(
+      {
+        type: 'text',
+        name: FIELD_NAMES.NAME,
+        id: FIELD_NAMES.NAME,
+        placeholder: 'Ivan',
+        class: styles.form__input,
+        autocomplete: 'off',
+      },
+      this.form,
+    );
+
+    const inputSurname = new InputFieldComponent(
+      {
+        type: 'text',
+        name: FIELD_NAMES.SURNAME,
+        id: FIELD_NAMES.SURNAME,
+        placeholder: 'Ivanov',
+        class: styles.form__input,
+        autocomplete: 'off',
+      },
+      this.form,
+    );
+
+    this.inputFieldsHTML.push(inputName.getHTML(), inputSurname.getHTML());
+    return [inputName, inputSurname];
+  }
+
+  private createSubmitBtn(): SubmitButtonComponent {
+    const submitBtn = new SubmitButtonComponent(
+      this.form,
+      'Login',
+      [styles.form__btn, 'btn-reset'],
+      {
+        type: 'submit',
+        disabled: 'true',
+      },
+    );
+
+    return submitBtn;
+  }
+
+  private createHTML(): HTMLFormElement {
+    const form = createBaseElement({
+      tag: 'form',
+      cssClasses: [styles.form],
+      attributes: {
+        action: '#',
+        method: 'post',
+      },
+    });
+
+    this.inputFields = this.createInputsField();
+
+    this.inputFieldsHTML.forEach((input) => {
+      const fieldBox = this.createFieldBox(input);
+      form.append(fieldBox);
+    });
+
+    form.append(this.submitBtn.getHTML());
+    return form;
+  }
+}
+
+export default LoginForm;
