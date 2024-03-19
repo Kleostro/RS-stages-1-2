c7d600bc07c4c9010585f208fe7c07ec3477fe1b
Max <133934232+Kleostro@users.noreply.github.com>
2024-03-19 20:57:27 +0300

    chore: add configurations for the async-race task

---
+++ async-race/vite.config.js
@@ -0,0 +1,44 @@
+import Inspect from 'vite-plugin-inspect';
+import { resolve } from 'path';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import autoprefixer from 'autoprefixer';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import checker from 'vite-plugin-checker';
+
+export default {
+  publicDir: 'assets',
+  plugins: [
+    Inspect(),
+    ViteImageOptimizer({
+      png: {
+        quality: 85,
+      },
+      jpg: {
+        quality: 85,
+      },
+      webp: {
+        quality: 70,
+      },
+    }),
+    tsconfigPaths(),
+    checker({
+      typescript: true,
+    }),
+  ],
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, './index.html'),
+      },
+    },
+    minify: false,
+    compact: false,
+    sourcemap: true,
+  },
+  css: {
+    postcss: {
+      plugins: [autoprefixer()],
+    },
+  },
+};
