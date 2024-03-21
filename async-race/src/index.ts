b133fbae146cf9473687e0e141204477b3e55e24
Max <133934232+Kleostro@users.noreply.github.com>
2024-03-21 03:44:52 +0300

    docs: delete async-race directory

--- async-race/src/index.ts
+++
@@ -1,5 +0,0 @@
-import AppModel from './app/App/model/AppModel.ts';
-import './index.scss';
-
-const myApp = new AppModel();
-document.body.append(myApp.getHTML());
--- async-race/src/pages/types/interfaces.ts
+++
@@ -1,7 +0,0 @@
-interface PageInterface {
-  getHTML(): HTMLDivElement;
-  hide(): void;
-  show(): void;
-}
-
-export default PageInterface;
